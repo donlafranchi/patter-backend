ccfcad537212cc1645bb296ad4b59c385342d6e3
IT-corridor <it.corridor051@gmail.com>
2020-01-03 11:15:34 -0800

    feat: add filters

--- general/views.py
+++ general/views.py
@@ -22,12 +22,18 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         qs = Event.objects.all()
+        q = self.request.GET.get('q')
+        if q:
+            qs = qs.filter(name__icontains=q)
+
         date = self.request.GET.get('date')
         if date:
             qs = qs.filter(date__date=datetime.strptime(date, '%Y-%m-%d'))
+
         category = self.request.GET.get('category')
         if category:
             qs = qs.filter(category=category)
+            
         venue_id = self.request.GET.get('venue')
         if venue_id:
             qs = qs.filter(venue_id=venue_id)
